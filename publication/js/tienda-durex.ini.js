c40893abb2f235ee2639c15095ebf3ac5f7968ec
Cristian Cortés <cristian.cortes@brm.com.co>
2016-04-07 17:41:51 -0500

    js para acciones de mi cuenta

--- publication/js/tienda-durex.ini.js
+++ publication/js/tienda-durex.ini.js
@@ -1,10 +1,69 @@
  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
 	screenWidth = $(window).width();
 
+
 jQuery(document).ready(function($) {
-	/*Scroll down*/
-	$('.scroll').click(function() {
-		$('html,body').animate({scrollTop:$(window).scrollTop()+580+'px'},1000);
+	
+	/*paneles de seccion "micuenta"*/
+	$('.botones-perfil .btn').click(function() {
+
+		var tipo = $(this).attr('data-btn'),
+			formCta = $('.cuenta');
+		
+		$('.botones-perfil .btn').removeClass('active');
+		$(this).addClass('active');
+
+		switch(tipo){
+
+			case 'perfil':
+				formCta.addClass('hidden');		
+				$('#datos-personales').removeClass('hidden');		
+			break;
+			
+			case 'preferencias':
+
+				formCta.addClass('hidden');		
+				$('#preferencias').removeClass('hidden');		
+			break;
+
+			case 'suscripcion':
+
+				formCta.addClass('hidden');		
+				$('.datos-sucripcion').removeClass('hidden');		
+				
+			break;
+
+			case 'ordenes':
+
+				formCta.addClass('hidden');		
+				$('.datos-ordenes').removeClass('hidden');		
+			break;
+
+
+		};
+
+		
 	});
+	/*paneles de seccion "micuenta"*/
+
+	/* play/pause estado de suscripcion*/
+
+	$('.pausar-suscripcion').click(function(event) {
+		event.preventDefault();
+
+		$(this).text('activar');
+		$('.suscripcion .status').toggleClass('status-paused');
+		$('.suscripcion .status p').text('pausada');
+
+		if( !$('.status').hasClass('status-paused') ){
+			$('.suscripcion .status p').text('activada');
+			$(this).text('pausar');
+		};
+
+
+
+	});
+
+	/* play/pause estado de suscripcion*/
 
 });
